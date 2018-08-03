1122c1c74b6487193b4828c0bc17fb88a2eaa4b3
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-03 12:59:48 +0300

    links generation added on button click

--- src/components/SendTab/SendTab.jsx
+++ src/components/SendTab/SendTab.jsx
@@ -8,10 +8,16 @@ import CheckBox from './../common/CheckBox';
 import { parse, format, asYouType } from 'libphonenumber-js';
 import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Error, ButtonLoader } from './../common/Spinner';
+import { SpinnerOrError, Loader } from './../common/Spinner';
 import WithHistory from './../HistoryScreen/WithHistory';
 import E2PCarousel from './../common/E2PCarousel';
 import { Row, Col } from 'react-bootstrap';
 import web3Service from './../../services/web3Service';
+const Wallet = require('ethereumjs-wallet');
+import ksHelper from '../../utils/keystoreHelper';
+const SIGNATURE_PREFIX = "\x19Ethereum Signed Message:\n32";
+const Web3Utils = require('web3-utils');
+import { CSVLink, CSVDownload } from 'react-csv';
 
 
 const styles = {
@@ -76,7 +82,12 @@ const styles = {
         fontFamily: 'SF Display Bold'
     },
     blue: '#0099ff',
-    blueOpacity: '#80ccff'
+    blueOpacity: '#80ccff',
+    button: {
+        width: '78%',
+        margin: 'auto'
+    },
+    green: '#2bc64f'    
 }
 
 
@@ -86,103 +97,119 @@ class Tab extends Component {
         this.state = {
             amount: 0,
             errorMessage: "",
-            fetching: false,
+
+	    //fetching: false,
             buttonDisabled: false,
-            checked: false,
-            checkboxTextColor: '#000',
-            numberInputError: false,
-            phoneError: false
+            //checked: false,
+            //checkboxTextColor: '#000',
+            //numberInputError: false,
+            //phoneError: false,
+
+
+	    linksGenerated: false,
+	    links: []
         };
     }
 
-    async _sendTransfer(phone, phoneCode) {
-        try {
-            const transfer = await this.props.sendTransfer({
-                amount: this.state.amount,
-                phone,
-                phoneCode
-            });
-            this.props.history.push(`/transfers/${transfer.id}`);
-        } catch (err) {
-            let errorMsg = err.message;
-            if (err.isOperational) errorMsg = "User denied transaction";
-            this.setState({ fetching: false, errorMessage: errorMsg });
-        }
+    // _createContract() {
+    // 	let abi = compiledContract.contracts['nameContract'].interface;
+    // 	let bytecode = compiledContract.contracts['nameContract'].bytecode;
+    // 	let gasEstimate = web3.eth.estimateGas({data: bytecode});
+    // 	let MyContract = web3.eth.contract(JSON.parse(abi));
 
-    }
+    // 	var myContractReturned = MyContract.new(param1, param2, {
+    // 	    from:mySenderAddress,
+    // 	    data:bytecode,
+    // 	    gas:gasEstimate}, function(err, myContract){
+    // 		if(!err) {
+    // 		    // NOTE: The callback will fire twice!
+    // 		    // Once the contract has the transactionHash property set and once its deployed on an address.
+
+    // 		    // e.g. check tx hash on the first call (transaction send)
+    // 		    if(!myContract.address) {
+    // 			console.log(myContract.transactionHash) // The hash of the transaction, which deploys the contract
+
+    // 			// check address on the second call (contract deployed)
+    // 		    } else {
+    // 			console.log(myContract.address) // the contract address
+    // 		    }
+
+    // 		    // Note that the returned "myContractReturned" === "myContract",
+    // 		    // so the returned "myContractReturned" object will also get the address set.
+    // 		}
+    // 	    });
+    // }
 
 
     _onSubmit() {
 
-        // hack for issue with phonenumber lib - https://github.com/bl00mber/react-phone-input-2/issues/10	
-        let phone = this.phoneNumber.state.formattedNumber;
-
-        // remove formatting from phone number
-        phone = "+" + phone.replace(/\D+/g, "");
-
-
-        // get dial code from phone number
-        const formatter = new asYouType();
-        formatter.input(phone);
-        const phoneCode = formatter.country_phone_code;
+	const dt = Date.now();
+	const { privateKey: masterPK, address: masterAddress } = this._generateAccount();
 	
-        //format balance
-        let balance;
-        const web3 = web3Service.getWeb3();
-        if (this.props.balanceUnformatted) {
-            balance = web3.fromWei(this.props.balanceUnformatted, 'ether').toNumber();
+	const n = 100;
+	let i = 0;
+	const links = [];
+	while (i < n) {	    
+	    const link = this._constructLink(masterPK, masterAddress);
+	    console.log(i + " -- " + link);
+	    links.push([link]);
+	    i++;
 	}
-
-        // check that phone number is valid
-	const isValidNumber = (phone || "").length >= 9;
-        if (!isValidNumber) {
-            this.setState({ fetching: false, errorMessage: "Phone number is invalid", phoneError: true });
-            return;
-        };
-
-        // check amount
-        if (this.state.amount <= 0) {
-            this.setState({ fetching: false, errorMessage: "Amount should be more than 0", numberInputError: true });
-            return;
-        };
-
-        // check amount maximum
-        if (this.state.amount > 1) {
-            this.setState({ fetching: false, errorMessage: (<span>*In beta you can send <span style={styles.betaBold}>1 ETH</span> max.</span>), numberInputError: true });
-            return;
-        };
-
-        // check wallet has enough ether
-        if (this.state.amount > balance) {
-            this.setState({ fetching: false, errorMessage: "Not enough ETH on your balance", numberInputError: true });
-            return;
-        };
-
-        // check if checkbox is submitted
-        if (this.state.checked === false) {
-            this.setState({ buttonDisabled: true, checkboxTextColor: '#e64437' })
-            return;
-        }
-
-        // disabling button
-        this.setState({ fetching: true });
-
-        // sending transfer
-        setTimeout(() => {  // let ui update
-            this._sendTransfer(phone, phoneCode);
-        }, 100);
+	const now = Date.now();
+	const diff = now - dt;
+	console.log({dt, diff, now});
+	this.setState({
+	    buttonDisabled: true,
+	    linksGenerated: true,
+	    links
+	});
     };
 
-
+    _constructLink(masterPK, masterAddress) {
+	const { address, privateKey } = this._generateAccount();
+	const verificationHash = Web3Utils.soliditySha3(SIGNATURE_PREFIX, { type: 'address', value: address });
+	const signature = ksHelper.signWithPK(masterPK, verificationHash);
+	const v = signature.v;
+	const r = '0x' + signature.r.toString("hex");
+	const s = '0x' + signature.s.toString("hex");
+	let link = `https://air.eth2.io/#/r?v=${v}&r=${r}&s=${s}&pk=${privateKey.toString('hex')}&c=${masterAddress}`;
+	return link;
+    }
+    
+    _generateAccount() {
+	const wallet = Wallet.generate();
+	const address = wallet.getChecksumAddressString();
+	const privateKey = wallet.getPrivateKey();
+	
+	return { address, privateKey };
+    }
     
     _renderForm() {
+	
         return (
             <Row>
-                <Col sm={4} smOffset={4}>
-                  <div>
-		    Eth2Air
-                  </div>
-                </Col>
+	      <Col sm={4} smOffset={4}>
+		<div style={styles.formContainer}>	    
+		<div style={styles.button}>
+		  <ButtonPrimary
+		     handleClick={this._onSubmit.bind(this)}
+		     disabled={this.state.buttonDisabled}		   
+		     buttonColor={styles.green}>
+		    Generate Links
+		  </ButtonPrimary>
+		</div>
+		<div style={{marginTop:100 }}>
+		  { this.state.links.length > 0 ?
+		    <div style={{textAlign: 'center'}}>
+			  <CSVLink data={this.state.links} filename="airdrop-links.csv">
+				Download CSV  </CSVLink>
+			</div>
+			: null
+		  }
+		</div> 				
+		<SpinnerOrError fetching={this.state.fetching} error={this.state.errorMessage}/>		    
+		</div>		
+	      </Col>
             </Row>
 
         );
