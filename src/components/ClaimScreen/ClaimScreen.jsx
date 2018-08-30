b8112578efec7506d796508fd7a5304307ccd7ad
Alexander Milhaylov <sashamihaha@gmail.com>
2018-08-30 22:37:44 +0300

    claim screen

--- src/components/ClaimScreen/ClaimScreen.jsx
+++ src/components/ClaimScreen/ClaimScreen.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Row, Col, Grid } from 'react-bootstrap';
 import Promise from 'bluebird';
 const qs = require('querystring');
-
+import RetinaImage from 'react-retina-image';
 import eth2air from 'eth2air-core';
 import ButtonPrimary from './../common/ButtonPrimary';
 import { SpinnerOrError, Loader } from './../common/Spinner';
@@ -18,76 +18,76 @@ class ClaimScreen extends Component {
     constructor(props) {
         super(props);
 
-	// parse URL params
-        const queryParams = qs.parse(props.location.search.substring(1));	
-	const { c: contractAddress, pk: transitPK, r: keyR, s: keyS, v: keyV } = queryParams;
-	
+        // parse URL params
+        const queryParams = qs.parse(props.location.search.substring(1));
+        const { c: contractAddress, pk: transitPK, r: keyR, s: keyS, v: keyV } = queryParams;
+
         this.state = {
-	    contractAddress,
-	    transitPK,
-	    keyR,
-	    keyS,
-	    keyV,
-	    loading: true,
+            contractAddress,
+            transitPK,
+            keyR,
+            keyS,
+            keyV,
+            loading: true,
             errorMessage: "",
             fetching: false,
-	    tokenSymbol: null,
-	    amount: null,
-	    tokenAddress: null,
-	    linkClaimed: false
+            tokenSymbol: null,
+            amount: null,
+            tokenAddress: null,
+            linkClaimed: false
         };
     }
 
     componentDidMount() {
-	this._getAirdropParams();
+        this._getAirdropParams();
     }
 
     async _getAirdropParams() {
-	try {
-	    const web3 = web3Service.getWeb3();
-	    
-	    // get airdrop params from the airdrop smart-contract
-	    const {
-		tokenSymbol,
-		claimAmount,
-		tokenAddress,
-		linkClaimed
-	    } = await eth2air.getAirdropParams({
-		contractAddress: this.state.contractAddress,
-		transitPK: this.state.transitPK,
-		web3
-	    });
+        try {
+            const web3 = web3Service.getWeb3();
 
-	    // update UI
-	    this.setState({
-		tokenSymbol,
-		amount: claimAmount,
-		tokenAddress,
-		linkClaimed,
-		loading: false
-	    });
-	} catch(err) {
-	    console.log(err);
-	    alert("Couldn't get airdrop details. Error details in the console.");
-	}
+            // get airdrop params from the airdrop smart-contract
+            const {
+                tokenSymbol,
+                claimAmount,
+                tokenAddress,
+                linkClaimed
+            } = await eth2air.getAirdropParams({
+                contractAddress: this.state.contractAddress,
+                transitPK: this.state.transitPK,
+                web3
+            });
+
+            // update UI
+            this.setState({
+                tokenSymbol,
+                amount: claimAmount,
+                tokenAddress,
+                linkClaimed,
+                loading: false
+            });
+        } catch (err) {
+            console.log(err);
+            alert("Couldn't get airdrop details. Error details in the console.");
+        }
     }
-    
+
     async _onSubmit() {
-	// disabling button
-	this.setState({fetching: true});
-	
+        // disabling button
+        this.setState({ fetching: true });
+
         try {
             const transfer = await this.props.claimTokens({
-		amount: this.state.amount,
-		tokenAddress: this.state.address,
-		tokenSymbol: this.state.tokenSymbol,
-		contractAddress: this.state.contractAddress,
-		transitPK: this.state.transitPK,
-		keyR: this.state.keyR,
-		keyS: this.state.keyS,
-		keyV: this.state.keyV
-	    });
-	    this.setState({fetching: false});
+                amount: this.state.amount,
+                tokenAddress: this.state.address,
+                tokenSymbol: this.state.tokenSymbol,
+                contractAddress: this.state.contractAddress,
+                transitPK: this.state.transitPK,
+                keyR: this.state.keyR,
+                keyS: this.state.keyS,
+                keyV: this.state.keyV
+            });
+            this.setState({ fetching: false });
 
             this.props.history.push(`/transfers/${transfer.id}`);
         } catch (err) {
@@ -95,57 +95,82 @@ class ClaimScreen extends Component {
             this.setState({ errorMessage: err.message, fetching: false });
         }
     }
-    
-    _renderConfirmDetailsForm() {		
-	// wait until loaded
-	if (this.state.loading) {
-	    return (<div>Loading...</div>);
-	}
-	
-	return (
-    <div style={{flexDirection: 'column', alignItems: 'center'}}>
-        <div style={{height: 250}}>
-	  <div style={styles.titleContainer}>
-	    <span style={styles.title}>Claim</span>
-	  </div>
-	  
-	  <div style={styles.amountContainer}>
-	    <span style={styles.amountNumber}>{this.state.amount} </span><span style={styles.amountSymbol}>{this.state.tokenSymbol}</span>
-	  </div>
-	  <div style={styles.formContainer}>	    
-	    <div style={styles.button}>
-	  { this.state.linkClaimed ? (<div className="text-center"> Link has been claimed </div>) : 	    
-	      <ButtonPrimary
-		 handleClick={this._onSubmit.bind(this)}
-		 disabled={this.state.fetching}		   
-		 buttonColor={styles.green}>
-		Confirm
+
+    isPrefixed(str = '') {
+        return str.slice(0, 2) === '0x';
+    }
+
+    dePrefix(str = '') {
+        if (this.isPrefixed(str)) {
+            return str.slice(2);
+        }
+        return str;
+    }
+
+    _shortAddress(address, num, showEnd = true) {
+        const sanitized = this.dePrefix(address);
+        const shorten = `${sanitized.slice(0, 3)}...${showEnd ? sanitized.slice(-num) : ''}`;
+        return '0x'.concat(shorten);
+    }
+
+    _renderConfirmDetailsForm() {
+        // wait until loaded
+        if (this.state.loading) {
+            return (<div>Loading...</div>);
+        }
+
+        return (
+            <div style={{ flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ height: 250 }}>
+                    <div><RetinaImage className="img-responsive" style={{ width: 100, height: 100, display: 'block', margin: 'auto', marginTop: 50 }} src={`https://trustwalletapp.com/images/tokens/${this.state.tokenAddress}.png `} />
+                    </div>
+
+                    <div style={styles.amountContainer}>
+                        <span style={styles.amountNumber}>{this.state.amount} </span><span style={styles.amountSymbol}>{this.state.tokenSymbol}</span>
+                    </div>
+                    <div style={{ textAlign: 'center' }}>
+                        <div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Medium', color: '#2BC64F' }}>+0.005 </div>
+                        <div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Bold' }}>ETH </div>
+                        <div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Regular' }}>to use token</div>
+                    </div>
+                    <div style={styles.formContainer}>
+                        <div style={styles.button}>
+                            {this.state.linkClaimed ? (<div className="text-center"> Link has been claimed </div>) :
+                                <ButtonPrimary
+                                    handleClick={this._onSubmit.bind(this)}
+                                    disabled={this.state.fetching}
+                                    buttonColor={styles.blue}>
+                                    Claim
 	    </ButtonPrimary>
-	  }	    
-	    </div> 		
-	    <SpinnerOrError fetching={this.state.fetching} error={this.state.errorMessage}/>
+                            }
+                        </div>
+                        <div style={{ textAlign: 'center', marginTop: 20 }}>
+                            <div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Regular' }}>Claiming to </div><div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Bold' }}>{this._shortAddress(this.props.claimAddress, 5)}</div>
+                        </div>
+                        <div style={{ textAlign: 'center', marginTop: 72 }}>
+                            <div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Medium', color: '#979797' }}>Powered by </div><div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Bold' }}>Eth2</div>
+                        </div>
+                        <SpinnerOrError fetching={this.state.fetching} error={this.state.errorMessage} />
+                    </div>
+                </div>
             </div>
-	</div>
-    </div>
-	);
+        );
     }
 
     render() {
         return (
-            <WithHistory {...this.props}>
-                <Grid>
-                    <Row>
-                      <Col sm={4} smOffset={4}>
+            <Grid>
+                <Row>
+                    <Col sm={4} smOffset={4}>
                         <div>
-			  { this._renderConfirmDetailsForm()}
+                            {this._renderConfirmDetailsForm()}
                         </div>
-                      </Col>
-                    </Row>
-                </Grid>
-            </WithHistory>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
 
 
-export default connect(state => ({ networkId: state.web3Data.networkId }), { claimTokens })(ClaimScreen);
+export default connect(state => ({ networkId: state.web3Data.networkId, claimAddress: state.web3Data.address }), { claimTokens })(ClaimScreen);
--- src/components/common/ButtonPrimary.jsx
+++ src/components/common/ButtonPrimary.jsx
@@ -8,15 +8,15 @@ class e2pButtonPrimary extends React.Component {
         return (
             <Button className="button-primary" disabled={this.props.disabled} style={{
 			width: '100%',
-			height: this.props.buttonHeight || 38,
-			borderRadius: 12,
+			height: this.props.buttonHeight || 50,
+			borderRadius: 10,
 			borderColor: this.props.buttonColor,                
 			backgroundColor: this.props.buttonColor,
 			opacity: this.props.disabled ? 0.5 : 1,
 			display: 'block', 
 			color: "#fff",
-			fontSize: this.props.fontSize ? this.props.fontSize : 18,
-			fontFamily: "SF Display Black",                 
+			fontSize: this.props.fontSize ? this.props.fontSize : 20,
+			fontFamily: "Inter UI Bold",                 
             }} onClick={this.props.handleClick}>
                 {this.props.children}
             </Button>
