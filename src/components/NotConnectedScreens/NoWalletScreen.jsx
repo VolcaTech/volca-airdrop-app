5df6aed8c452e8c02ec52eb76a5f651a464fb049
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-30 23:32:11 +0300

    Other wallets removed

--- src/components/NotConnectedScreens/NoWalletScreen.jsx
+++ src/components/NotConnectedScreens/NoWalletScreen.jsx
@@ -114,72 +114,21 @@ class NoWalletScreen extends Component {
         return (
             <div>
                 <div style={styles.title}>You need wallet to<br />use the service</div>
-                {window.innerWidth < 769 ?
-                    (
-                        <div>
-                            <div style={{ ...styles.instructionsText, textAlign: 'center' }}> We recommend Trust Wallet </div>
-                            <div style={styles.instructionsContainer}>
-                                <div style={{ ...styles.instructionsText, fontFamily: 'SF Display Bold' }}>How to:</div>
-                                <div style={styles.instructionsText}> 1. Download <div style={styles.instructionsTextBold}>Trust Wallet</div> (button below) </div>
-                                <div style={styles.instructionsText}> 2. Create new or import existing wallet </div>
-                                <div style={styles.instructionsText}> 3. Receive Ether (link will be open automatically) </div>
-                            </div>
-                            <div style={styles.buttonRow}>
-                                <a className={`btn btn-primary ${disabled}`} href={this.state.deepLink || "#"} style={styles.button}> Use Trust Wallet </a>
-				<Link to="faq"><RetinaImage src="https://eth2.io/images/q.png" /> </Link>
-                            </div>
-                        </div>
-                    ) :
-                    (
-                        <div>
-                            <div style={{ ...styles.instructionsText, textAlign: 'center' }}> On desktop we recommend Metamask </div>
-                            <div style={styles.instructionsContainer}>
-                                <div style={{ ...styles.instructionsText, fontFamily: 'SF Display Bold' }}>How to:</div>
-                                <div style={styles.instructionsText}> 1. Install Metamask Chrome Extension</div>
-                                <div style={styles.instructionsText}> 2. Create new or import existing wallet </div>
-                                <div style={styles.instructionsText}> 3. Receive Ether (link will be reload automatically) </div>
-                            </div>
-                            <div style={styles.buttonRow}>
-                                <a href="https://metamask.io/" style={{...styles.button, backgroundColor: '#f5a623', borderColor: '#f5a623'}} target="_blank"> Install Metamask </a>
-                            </div>
-                        </div>
-                    )}
-                <WalletsList />
+                <div>
+                  <div style={{ ...styles.instructionsText, textAlign: 'center' }}> We recommend Trust Wallet </div>
+                  <div style={styles.instructionsContainer}>
+                    <div style={{ ...styles.instructionsText, fontFamily: 'SF Display Bold' }}>How to:</div>
+                    <div style={styles.instructionsText}> 1. Download <div style={styles.instructionsTextBold}>Trust Wallet</div> (button below) </div>
+                    <div style={styles.instructionsText}> 2. Create new or import existing wallet </div>
+                    <div style={styles.instructionsText}> 3. Receive Ether (link will be open automatically) </div>
+                  </div>
+                  <div style={styles.buttonRow}>
+                    <a className={`btn btn-primary ${disabled}`} href={this.state.deepLink || "#"} style={styles.button}> Use Trust Wallet </a>
+                  </div>
+                </div>
             </div>
         );
     }
 }
-
-
-const WalletsList = () => {
-    return (
-        <div>
-            <Row style={styles.row}>
-                <div style={styles.supported}>Supported wallets</div>
-            </Row>
-
-            <div style={{...styles.row, maxWidth: 400}}>
-                <div style={styles.walletLogoContainer}>
-		  <RetinaImage className="img-responsive" style={styles.logo} src="https://eth2.io/images/trust.png" />		  
-                    <div style={styles.logoText}>Trust</div>
-                </div>
-                <div style={styles.walletLogoContainer}>
-		  <RetinaImage className="img-responsive" style={styles.logo} src="https://eth2.io/images/metamask.png" />		  		  
-                    <div style={styles.logoText}>Metamask</div>
-                </div>
-                <div style={styles.walletLogoContainer}>
-		  <RetinaImage className="img-responsive" style={styles.logo} src="https://eth2.io/images/toshi.png" />		  		  
-                  <div style={styles.logoText}>Toshi</div>
-                </div>
-                <div style={styles.walletLogoContainer}>
-		  <RetinaImage className="img-responsive" style={styles.logo} src="https://eth2.io/images/token_pocket.png" />		  		  
-                    <div style={styles.logoText}>Token Pocket</div>
-                </div>
-		
-            </div>
-        </div>
-    )
-}
-
 
 export default NoWalletScreen;
