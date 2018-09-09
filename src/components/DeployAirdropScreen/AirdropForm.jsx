c8bc7897d336367e8fec381d2ac3f6feb0b72f82
Alexander Milhaylov <sashamihaha@gmail.com>
2018-09-10 01:53:13 +0300

    tokens select list logic

--- src/components/DeployAirdropScreen/AirdropForm.jsx
+++ src/components/DeployAirdropScreen/AirdropForm.jsx
@@ -1,71 +1,106 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import Promise from 'bluebird';
 const erc20abi = require('human-standard-token-abi');
 import web3Service from './../../services/web3Service';
+import Select from 'react-select'
 import styles from './styles';
 
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+]
+
+const web3 = web3Service.getWeb3();
 
 class AirdropForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tokensOfAddress: []
+        };
+    }
+
+    componentDidMount() {
+        this._getTokensOfAddress(this.props.address).then(tokens => this.setState({ tokensOfAddress: tokens }))
+    }
 
     _getToken(tokenAddress) {
-	const web3 = web3Service.getWeb3();
         const instance = web3.eth.contract(erc20abi).at(tokenAddress);
-	Promise.promisifyAll(instance, { suffix: 'Promise' });
-	return instance;
+        Promise.promisifyAll(instance, { suffix: 'Promise' });
+        return instance;
     }
 
-    async _onTokenAddressChange(tokenAddress)    {
-	this.props.updateForm({
-	    tokenAddress
-	});	    
-
-	if (tokenAddress.length === 42) {
-	    try { 
-		
-		const token = this._getToken(tokenAddress);
-		
-		const tokenName = await token.namePromise();
-		
-		let tokenDecimals = await token.decimalsPromise();
-		tokenDecimals = tokenDecimals.toNumber();
-		
-		let tokenSymbol = await token.symbolPromise();
-		
-		const web3 = web3Service.getWeb3();
-		let tokenBalance = await token.balanceOfPromise(web3.eth.accounts[0]);
-		tokenBalance = tokenBalance.shift(-1*tokenDecimals).toNumber();
-		
-		this.props.updateForm({
-		    tokenBalance,
-		    tokenName,
-		    tokenDecimals,
-		    tokenSymbol
-		});
-	    } catch (err) {
-		console.log(err);
-		alert("Error while getting token details from the blockchain. More info in the console.");
-	    }
-	}	
+    async _getTokensOfAddress(address) {
+        let tokensOfAddressArrayFormatted = [];
+        const tokensOfAddress = await fetch(`https://api.trustwalletapp.com/tokens?address=${address}`).then(result => {
+            return result.json()
+        })
+        const tokensOfAddressArray = tokensOfAddress.docs
+        tokensOfAddressArray.map(token => {
+            tokensOfAddressArrayFormatted.push({ value: token.contract.symbol, label: `${token.contract.symbol}--${token.contract.address}` })
+        })
+        return tokensOfAddressArrayFormatted
     }
 
-    
-   render() {
-       return (
-	   <div style={{ marginBottom: 50}}>	   
-	    <Row>
-	      <Col sm={12}>
-		<div style={{margin: 10}}>
+    async _onTokenAddressChange(tokenAddress) {
+        this.props.updateForm({
+            tokenAddress
+        });
+
+
+
+        if (tokenAddress.length === 42) {
+            try {
+
+                const token = this._getToken(tokenAddress);
+
+                const tokenName = await token.namePromise();
+
+                let tokenDecimals = await token.decimalsPromise();
+                tokenDecimals = tokenDecimals.toNumber();
+
+                let tokenSymbol = await token.symbolPromise();
+
+                const web3 = web3Service.getWeb3();
+                let tokenBalance = await token.balanceOfPromise(web3.eth.accounts[0]);
+                tokenBalance = tokenBalance.shift(-1 * tokenDecimals).toNumber();
+
+                this.props.updateForm({
+                    tokenBalance,
+                    tokenName,
+                    tokenDecimals,
+                    tokenSymbol
+                });
+            } catch (err) {
+                console.log(err);
+                alert("Error while getting token details from the blockchain. More info in the console.");
+            }
+        }
+    }
+
+
+    render() {
+
+        console.log(this.state.tokensOfAddress)
+        console.log(options)
+        return (
+            <div style={{ marginBottom: 50 }}>
+                <Row>
+                    <Col sm={12}>
+                        {/* <div style={{margin: 10}}>
 		  <label>Token Address:</label>
 		  <input className="form-control" value={this.props.tokenAddress} onChange={({target}) => this._onTokenAddressChange(target.value)}/>
-		</div>
+		</div> */}
 
-		<div style={{margin: 10}}>
-		  <label>Token Name: </label>
-		  <span> {this.props.tokenName}</span>
-		</div>
+                        <div style={{ margin: 10 }}>
+                            <label>Token: </label>
+                            <Select indicatorSeparator={false} options={this.state.tokensOfAddress} />
+                        </div>
 
-		<div style={{margin: 10}}>
+                        {/* <div style={{margin: 10}}>
 		  <label>Token Decimals: </label>
 		  <span> {this.props.tokenDecimals}</span>
 		</div>
@@ -73,45 +108,46 @@ class AirdropForm extends Component {
 		<div style={{margin: 10}}>
 		  <label>Balance: </label>
 		  <span> {this.props.tokenBalance} {this.props.tokenSymbol}</span>
-		</div>
-		<div style={{margin: 10}}>
-		  <label> Number of links: </label>
-		  <input className="form-control" type="number" value={this.props.linksNumber} onChange={({target}) => this.props.updateForm({linksNumber: target.value})}/>
-		</div>
-		
-		
-		<div style={{margin: 10}}>
-		  <label>Claim Amount of {this.props.tokenSymbol} per link:</label>
-		  <input className="form-control" type="number" value={this.props.claimAmount} onChange={({target}) => this.props.updateForm({claimAmount: target.value})}/>
-		</div>	
+		</div> */}
+                        <div style={{ margin: 10 }}>
+                            <label> Number of links: </label>
+                            <input className="form-control" type="number" value={this.props.linksNumber} onChange={({ target }) => this.props.updateForm({ linksNumber: target.value })} />
+                        </div>
 
-		<div style={{margin: 10}}>
-		  <label>Claim Amount of ETH per link:</label>
-		  <input className="form-control" type="number" value={this.props.claimAmountEth} onChange={({target}) => this.props.updateForm({claimAmountEth: target.value})}/>
-		</div>	
 
-		<div style={{margin: 10}}>
-		  <label>Eth cost:</label>
-		  <span> {this.props.claimAmountEth * this.props.linksNumber}</span>
-		</div>
-		
+                        <div style={{ margin: 10 }}>
+                            <label>Claim Amount of {this.props.tokenSymbol} per link:</label>
+                            <input className="form-control" type="number" value={this.props.claimAmount} onChange={({ target }) => this.props.updateForm({ claimAmount: target.value })} />
+                        </div>
 
-		<div style={styles.button}>
-		  <button
-		     className="btn btn-default"
-		     onClick={this.props.onSubmit}
-		     disabled={this.props.creationTxHash}		   
-		     >			
-		    1. Deploy AirDrop Contract
+                        <div style={{ margin: 10 }}>
+                            <label>Claim Amount of ETH per link:</label>
+                            <input className="form-control" type="number" value={this.props.claimAmountEth} onChange={({ target }) => this.props.updateForm({ claimAmountEth: target.value })} />
+                        </div>
+
+                        <div style={{ margin: 10 }}>
+                            <label>Eth cost:</label>
+                            <span> {this.props.claimAmountEth * this.props.linksNumber}</span>
+                        </div>
+
+
+                        <div style={styles.button}>
+                            <button
+                                className="btn btn-default"
+                                onClick={this.props.onSubmit}
+                                disabled={this.props.creationTxHash}
+                            >
+                                1. Deploy AirDrop Contract
 		  </button>
-		  </div>
-	      </Col>
-	    </Row>
-	    <hr/>			    
-	   </div>	   
-	);
+                        </div>
+                    </Col>
+                </Row>
+                <hr />
+            </div>
+        );
     }
 }
 
-
-export default AirdropForm;
+export default connect(state => ({
+    address: state.web3Data.address
+}))(AirdropForm);
