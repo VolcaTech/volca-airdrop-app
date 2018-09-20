95f2fc8bfca13efbd51809f17e109b95a0156b4d
Alexander Milhaylov <sashamihaha@gmail.com>
2018-09-20 21:09:15 +0300

    referal link sign in screen

--- src/components/AuthScreen/AuthScreen.jsx
+++ src/components/AuthScreen/AuthScreen.jsx
@@ -29,7 +29,7 @@ class ClaimScreen extends Component {
 
         this.state = {
             contractAddress,
-	    referralAddress,	    
+            referralAddress,
             loading: true,
             errorMessage: "",
             fetching: false,
@@ -58,7 +58,7 @@ class ClaimScreen extends Component {
                 transitPK: this.state.transitPK,
                 web3
             });
-	    
+
             // update UI
             this.setState({
                 tokenSymbol,
@@ -66,7 +66,7 @@ class ClaimScreen extends Component {
                 tokenAddress,
                 loading: false
             });
-	    
+
         } catch (err) {
             console.log(err);
             alert("Couldn't get airdrop details. Error details in the console.");
@@ -75,75 +75,75 @@ class ClaimScreen extends Component {
 
 
     async onGoogleResponse(response) {
-	console.log({response, state: this.state});
-	
-	try { 
-	    const authResult = await authenticate({
-		googleTokenId: response.tokenId,
-		referralAddress: this.state.referralAddress,
-		contractAddress: this.state.contractAddress	    
-	    });
-	    console.log({authResult});
-	    if (authResult.success && authResult.link) {
-		window.location.href = authResult.link;
-	    }
-	    
-	} catch(err) {
-	    console.log(err)
-	    alert("Error while authenticating");
-	}
+        console.log({ response, state: this.state });
+
+        try {
+            const authResult = await authenticate({
+                googleTokenId: response.tokenId,
+                referralAddress: this.state.referralAddress,
+                contractAddress: this.state.contractAddress
+            });
+            console.log({ authResult });
+            if (authResult.success && authResult.link) {
+                window.location.href = authResult.link;
+            }
+
+        } catch (err) {
+            console.log(err)
+            alert("Error while authenticating");
+        }
     }
 
-    
+
     _renderConfirmDetailsForm() {
         // wait until loaded
         if (this.state.loading) {
-            return (<Loader text="Getting airdrop details..." textLeftMarginOffset={-50}/>);
+            return (<Loader text="Getting airdrop details..." textLeftMarginOffset={-50} />);
         }
 
         return (
             <div style={{ flexDirection: 'column', alignItems: 'center' }}>
                 <div style={{ height: 250 }}>
-                    <RetinaImage className="img-responsive" style={{...styles.tokenIcon, borderRadius: 50, WebkitBoxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)'}} src={this.state.imageExists ? `https://trustwalletapp.com/images/tokens/${this.state.tokenAddress}.png` : 'https://raw.githubusercontent.com/Eth2io/eth2-assets/master/images/default_token.png'} onError={(e) => {this.setState({imageExists: false})}}/>
+                    <RetinaImage className="img-responsive" style={{ ...styles.tokenIcon}} src={`https://raw.githubusercontent.com/Eth2io/eth2-assets/master/images/doge_token.png`} onError={(e) => { this.setState({ imageExists: false }) }} />
 
-                    <div style={styles.amountContainer}>
-                        <span style={styles.amountNumber}>{this.state.amount} </span><span style={styles.amountSymbol}>{this.state.tokenSymbol}</span>
+                    <div style={{ ...styles.amountContainer, width: 300, margin: 'auto' }}>
+                        <div style={{ ...styles.title, fontFamily: 'Inter UI Black' }}>You need to sign in to claim <span style={styles.amountSymbol}><span style={{ fontFamily: 'Inter UI Bold' }}>{this.state.amount}</span> {this.state.tokenSymbol}</span><span style={{ fontFamily: 'Inter UI Bold' }}> ($25)</span></div>
                     </div>
                     <div style={styles.formContainer}>
                         <div style={styles.button}>
-
-			     <GoogleLogin
-			     clientId="954902551746-leebjqk6hs426eivvvvbicr1adntat9s.apps.googleusercontent.com"
-			     buttonText="Login"
-				onSuccess={this.onGoogleResponse.bind(this)}
-				onFailure={this.onGoogleResponse.bind(this)}
-			       />
-	    
+                            <GoogleLogin style={{ width: 300, height: 50, paddingLeft: 20, paddingRight: 20, display: 'flex', justifyContent: 'space-between', backgroundColor: 'white', borderWidth: 1, borderColor: '#979797', borderRadius: 10, fontSize: 20, fontFamily: 'Inter UI Bold' }}
+                                clientId="954902551746-leebjqk6hs426eivvvvbicr1adntat9s.apps.googleusercontent.com"
+                                onSuccess={this.onGoogleResponse.bind(this)}
+                                onFailure={this.onGoogleResponse.bind(this)}>
+                                <RetinaImage className="img-responsive" src={`https://raw.githubusercontent.com/Eth2io/eth2-assets/master/images/google_icon.png`} style={{display: 'inline'}} onError={(e) => { this.setState({ imageExists: false }) }} />
+                                Sign in with Google
+                   </GoogleLogin>
                         </div>
-                <div style={{ textAlign: 'center', marginTop: 20 }}>
-	    
-	    </div>
-                <SpinnerOrError fetching={false} error={this.state.errorMessage} />		
-            </div>		
+                        <div style={{fontFamily: 'Inter UI Regular', fontSize: 14, color: '#979797', textAlign: 'center', marginTop: 20}}>We ask email, photo and name</div>
+                        <div style={{ textAlign: 'center', marginTop: 20 }}>
+
+                        </div>
+                        <SpinnerOrError fetching={false} error={this.state.errorMessage} />
+                    </div>
+                </div>
             </div>
-	    </div>
         );
     }
 
     render() {
         return (
-	    <div>
-              <Grid>
-                <Row>
-                  <Col sm={4} smOffset={4}>
-                    <div>
-                      {this._renderConfirmDetailsForm()}
-                    </div>
-                  </Col>
-                </Row>
-              </Grid>
-	      <PoweredByEth2/>					
-	    </div>
+            <div>
+                <Grid>
+                    <Row>
+                        <Col sm={4} smOffset={4}>
+                            <div>
+                                {this._renderConfirmDetailsForm()}
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
+                <PoweredByEth2 />
+            </div>
         );
     }
 }
