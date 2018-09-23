1a0ec3c2dced75375443891e1e43e4c359a7b03b
Alexander Milhaylov <sashamihaha@gmail.com>
2018-09-23 21:32:16 +0300

    powered by volca fix

--- src/components/common/poweredByVolca.jsx
+++ src/components/common/poweredByVolca.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const PoweredByVolca = () => {
     return (
-	<div>
-	<div style={{ position: 'absolute', bottom: 20, width: '100%'}}>
+	<div style={{ bottom: 20, width: '100%', marginBottom: 30, marginTop: 30}}>
 	  <div style={{ width: '100%', textAlign:'center'}}>
           <a href="https://volca.tech" style={{ textDecoration: 'none'}}>
             <div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Medium', color: '#979797' }}>Powered by </div><div style={{ display: 'inline', fontSize: 18, fontFamily: 'Inter UI Bold', color: 'black' }}>Volcà</div>
         </a>
-	</div>
 	</div>
 	</div>
     )
