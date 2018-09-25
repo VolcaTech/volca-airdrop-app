a26a6b850caf20a034fd3397dfd49a97dfcda3bc
Alexander Milhaylov <sashamihaha@gmail.com>
2018-09-25 04:38:42 +0300

    nowallet screen fix in progress

--- src/components/NotConnectedScreens/WalletSlider.jsx
+++ src/components/NotConnectedScreens/WalletSlider.jsx
@@ -4,42 +4,7 @@ import Slider from "react-slick";
 import RetinaImage from 'react-retina-image';
 import wallets from './wallets';
 import { getDeviceOS } from './../../utils';
-
-
-const styles = {
-    logoText: {
-        textAlign: 'center',
-        fontSize: 14,
-        fontFamily: 'SF Display Regular',
-        marginTop: -6
-    },
-    logo: {
-        height: 60,
-        width: 60,
-        margin: 7,
-        borderRadius: 15,
-        WebkitBoxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)'
-    },
-    logo5: {
-        height: 50,
-        width: 50,
-        margin: 7,
-        borderRadius: 12.5,
-        WebkitBoxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)'
-    },
-    title: {
-        textAlign: 'center',
-        fontFamily: "SF Display Semibold",
-        fontSize: 20,
-        marginBottom: 25,
-        marginTop: 20
-    },
-    containerStyle: {
-
-    },
-    nextArrow: { width: 20, height: 20, position: 'absolute', top: 25, right: 0, textAlign: 'right' },
-    prevArrow: { height: 20, float: 'left', top: 25, paddingTop: 26 }
-}
+import styles from './styles';
 
 
 class WalletSlider extends React.Component {
@@ -76,7 +41,7 @@ class WalletSlider extends React.Component {
 	
         return (
             <div style={{ padding: 10 }}>
-                <div style={styles.title}>Choose your wallet:</div>
+                <div style={styles.chooseWallet}>Choose your wallet:</div>
                 <Slider {...settings}
                 >
                   {Object.keys(wallets)
