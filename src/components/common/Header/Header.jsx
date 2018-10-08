0c1b6e5c06bacbd051be946bcd79dea8f0d07223
Alexander Milhaylov <sashamihaha@gmail.com>
2018-10-08 19:13:51 +0300

    deploy airdrop header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -10,11 +10,15 @@ class e2pHeader extends React.Component {
         console.log(location.pathname)
         return (
             <Grid className='header'>
-                <Row className="header-row">
-                    <Col xs={5} style={{ paddingTop: 14 }}>
+                <Row className="header-row" style={{ paddingTop: 114 }}>
+                    <Col xs={5}>
                         <Logo />
                     </Col>
-                    <Col style={{paddingTop: 20}} xs={7}>    
+                    <Col style={{}} xs={7}>
+                    <div style={{display: 'flex', justifyContent: 'center', fontSize: 16, fontFamily: 'Inter UI Regular'}}>
+                        <div style={{paddingTop: 10}}>Unlock more features</div>
+                        <a href="mailto: hi@volca.tech" style={{width: 120, height: 32, marginTop: 5, marginLeft: 30, borderRadius: 5, border: 'none', backgroundColor: '#0078FF', color: 'white', textAlign: 'center', paddingTop: 5, textDecoration: 'none'}}>Contact Us</a>
+                        </div>
                     </Col>
                 </Row>
                 
--- src/components/common/Header/logo.jsx
+++ src/components/common/Header/logo.jsx
@@ -6,9 +6,13 @@ class Logo extends React.PureComponent {
 
     render() {
         return (
-            <div className="logo">
-                <span>Eth2</span>Airdrop
-		</div>
+            <div style={{display: 'flex'}}>
+                <div style={{ borderRight: '1px #DADADA solid' }}>
+                    <div style={{ display: 'inline-block', width: 32, height: 32, backgroundColor: '#0078FF', borderRadius: 16, verticalAlign: 'text-top', marginRight: 10 }}></div>
+                    <div style={{ display: 'inline', fontFamily: 'Inter UI Bold', fontSize: 26, verticalAlign: 'bottom', marginRight: 10 }}>volcà</div>
+                </div>
+                <div style={{paddingTop: 10, paddingLeft: 10, fontFamily: 'Inter UI Regular', fontSize: 14, color: '#979797'}}>Demo with 30 free claim links</div>
+            </div>
         );
     }
 }
