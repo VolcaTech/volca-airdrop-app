89e5dd5b2dfca30ee737f5104846217940d4d694
Alexander Milhaylov <sashamihaha@gmail.com>
2018-10-09 05:25:41 +0300

    smart contract created

--- src/components/DeployAirdropScreen/components.jsx
+++ src/components/DeployAirdropScreen/components.jsx
@@ -3,6 +3,8 @@ import { CSVLink, CSVDownload } from 'react-csv';
 import { getEtherscanLink } from './../Transfer/components';
 import { SpinnerOrError, Loader } from './../common/Spinner';
 import styles from './styles';
+import RetinaImage from 'react-retina-image';
+
 
 export const DownloadLinksButton = ({ links }) => {
 
@@ -23,13 +25,19 @@ export const DownloadLinksButton = ({ links }) => {
 }
 
 
-const TxDetails = ({ txHash, networkId, contractAddress }) => {
+const StatusDetailsAndApproveButton = ({ txHash, networkId, contractAddress, onSubmit, disabled }) => {
     //if (!txHash) { return null; }
     let stepLabel = "Deploy Tx: ";
     if (!contractAddress) {
-        stepLabel = 'Creating Smart Contract';
+        disabled=true
     }
-
+    let buttonColor;
+    if (disabled) {
+        buttonColor='#B2B2B2'
+    }
+    else {
+        buttonColor='#0078FF'
+    }
     const etherscanLink = getEtherscanLink({ txHash, networkId });
     return (
         <div>
@@ -37,55 +45,58 @@ const TxDetails = ({ txHash, networkId, contractAddress }) => {
                 <div style={{ fontFamily: 'Inter UI Regular', color: '#979797', marginRight: 10 }}>2/3</div>
                 <div style={{ fontFamily: 'Inter UI Black', color: '#0099FF', }}>Approve smart contract</div>
             </div>
-            <div style={{ ...styles.airdropBalanceContainer, width: 670, height: 238, flexDirection: 'column', padding: '40px 0px 40px 40px' }}>
+            <div style={{ ...styles.airdropBalanceContainer, width: 850, height: 238, display: 'block', flexDirection: 'column', padding: '40px 0px 40px 40px' }}>
                 {!contractAddress ?
                     <div style={{height: 30, marginBottom: 25, display: 'flex', fontSize: 20, fontFamily: 'Inter UI Medium'}}>
                         <div style={{marginRight: 15}}>Creating Smart Contract</div>
                         <Loader _className='' text="" size="small" />
                         {/* <a href={etherscanLink} className="link" target="_blank">{txHash}</a> */}
                     </div>
-                    : null}
-                    <div style={{marginBottom: 20, fontFamily: 'Inter UI Regular', fontSize: 18}}>Setup TX: <a href={etherscanLink} style={{color: '#0078FF', textDecoration: 'none'}} target="_blank">123456</a></div>
+                    : 
+                    <div style={{height: 30, marginBottom: 25, display: 'flex', fontSize: 20, fontFamily: 'Inter UI Medium'}}>
+                        <div style={{marginRight: 15}}>Smart Contract created</div>
+                        <RetinaImage src={`https://raw.githubusercontent.com/Eth2io/eth2-assets/master/images/done_small.png`} style={{ display: 'inline', height:'fit-content'}} />                        
+                    </div>
+                    }
+                    <div style={{marginBottom: 20, fontFamily: 'Inter UI Regular', fontSize: 18}}>Setup TX: <a href={etherscanLink} style={{color: '#0078FF', textDecoration: 'none'}} target="_blank">{txHash}</a></div>
                     <div style={{marginBottom: 30, fontFamily: 'Inter UI Regular', fontSize: 18}}>Smart Contract: {contractAddress ? <a href={etherscanLink} style={{color: '#0078FF', textDecoration: 'none'}} target="_blank">{contractAddress}</a> : <span style={{color: '#979797'}}>Transaction is processing</span>}</div>            
                     <div style={{fontFamily: 'Inter UI Regular', fontSize: 14, color: '#979797'}}>It may take a few minutes, don't close this page</div>
             </div>
-        </div>
-    );
-}
-
-
-const ContractDetailsAndApproveButton = ({ contractAddress, networkId, disabled, onSubmit }) => {
-    if (!contractAddress) { return null; }
-    const etherscanLink = getEtherscanLink({ address: contractAddress, networkId });
-
-    return (
-        <div>
-            <div> Airdrop Contract created at: <a href={etherscanLink} className="link" target="_blank">{contractAddress}</a> </div>
-
-            <div style={{ marginTop: 50 }}>
-                <div style={styles.button}>
+            <div style={styles.button}>
                     <button
-                        className="btn btn-default"
+                        style={{width: 250, height: 45, margin: 40, borderWidth: 0, borderRadius: 5, backgroundColor: buttonColor, textAlign: 'center', fontFamily: 'Inter UI Medium', fontSize: 20, color: 'white'}}
                         onClick={onSubmit}
                         disabled={disabled}
                     >
-                        2. Approve Contract and Generate Links
+                        Approve
 	      </button>
                 </div>
-            </div>
-            <hr />
         </div>
     );
 }
 
 
-export const ContractDetails = ({ contractAddress, networkId, disabled, onSubmit, txHash }) => (
-    <div>
+// const ContractDetails = ({ contractAddress, networkId, disabled, onSubmit }) => {
+//     if (!contractAddress) { return null; }
+//     const etherscanLink = getEtherscanLink({ address: contractAddress, networkId });
 
-        <TxDetails txHash={txHash} networkId={networkId} contractAddress={contractAddress} />
-        <ContractDetailsAndApproveButton contractAddress={contractAddress}
-            networkId={networkId}
-            onSubmit={onSubmit}
-            disabled={disabled} />
+//     return (
+//         <div>
+//             <div> Airdrop Contract created at: <a href={etherscanLink} className="link" target="_blank">{contractAddress}</a> </div>
+
+//             <div style={{ marginTop: 50 }}>
+                
+//             </div>
+//             <hr />
+//         </div>
+//     );
+// }
+
+
+export const ContractDetails = ({ contractAddress, networkId, disabled, onSubmit, txHash, links }) => (
+    <div>
+        {links.length > 0 && contractAddress ? 
+        <DownloadLinksButton links={links}/> :
+        <StatusDetailsAndApproveButton txHash={txHash} networkId={networkId} contractAddress={contractAddress} onSubmit={onSubmit} disabled={disabled}/>}
     </div>
 );
