650cdbc49d3e482e5e434a5add8d520c4978b2d7
Itay Radotzki <radotzki@gmail.com>
2019-02-02 17:06:31 +0200

    fix showPortis

--- src/services/web3Service.js
+++ src/services/web3Service.js
@@ -5,13 +5,15 @@ const BigNumber = require('bignumber.js');
 
 
 const Web3Service = () => {
-    let web3;
+	let web3;
+	let portis;
 
     async function setupPortisWeb3() {
-	const web3 = getPortisWeb3();
+	const { web3, portisInstance } = getPortisWeb3();
+	portis = portisInstance;
 	return await setup(web3);
     }
-    
+
     async function setup(_web3=null) {
 	// Get network provider and web3 instance.
 	// See utils/getWeb3 for more info.
@@ -19,10 +21,10 @@ const Web3Service = () => {
 	if (!web3) {
 	    throw new Error("Web3 is not connected");
 	}
-	
+
 	console.log("Web3 injected: ", {web3});
 	Promise.promisifyAll(web3.eth, { suffix: "Promise" });
-	Promise.promisifyAll(web3.version, { suffix: "Promise" });	
+	Promise.promisifyAll(web3.version, { suffix: "Promise" });
 	web3.eth.getTransactionReceiptMined = getTransactionReceiptMined;
 
 
@@ -30,17 +32,17 @@ const Web3Service = () => {
 	const network = await web3.version.getNetworkPromise();
 	const address = accounts[0];
 
-    
+
 	const balanceBig = address ? await web3.eth.getBalancePromise(address) : 0;
 	let balance;
 	if (balanceBig) {
 	    console.log({web3});
             balance = web3.fromWei(balanceBig, 'ether');
 	}
-	
-	const connected = web3.isConnected();	
+
+	const connected = web3.isConnected();
 	const { networkName, networkId } = detectNetwork(web3);
-	
+
 	return {
 	    web3,
 	    balance,
@@ -52,14 +54,14 @@ const Web3Service = () => {
     }
 
     function isPortis() {
-	return web3.currentProvider.isPortis;	
+	return web3.currentProvider.isPortis;
     }
 
-    function showPortisModal(cb) {
-	web3.currentProvider.showPortis(cb);	
+    function showPortisModal() {
+		portis.showPortis();
     }
-    
-    
+
+
     // api
     return {
 	showPortisModal,
--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -10,12 +10,12 @@ export const getPortisWeb3 = () => {
     let network = 'mainnet';
     if (String(queryParams.n) === '3') {
 	network = 'ropsten';
-    } 
+    }
     const portis = new Portis('df0b262e-623b-4846-b0bd-7dec748869bb', network);
-    
+
     const web3 = new Web3(portis.provider);
-	    
-    return web3;
+
+    return { web3, portisInstance: portis };
 }
 
 
@@ -23,8 +23,8 @@ const getWeb3 = () => {
     return new Promise((resolve, reject) => {
 	// Wait for loading completion to avoid race conditions with web3 injection timing.
 	const _getWeb3 = () => {
-	    var web3 = window.web3;	    
-	    if (window.ethereum) { 
+	    var web3 = window.web3;
+	    if (window.ethereum) {
 		window.ethereum.enable().then((result) => {
 		    const provider = window.ethereum;
 		    web3 = new Web3(provider);
@@ -42,7 +42,7 @@ const getWeb3 = () => {
 	};
 
 	window.addEventListener('load', _getWeb3);
-	
+
     });
 }
 
