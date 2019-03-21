de53569746dd1463f91dba7609a0e2fd655d3867
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-03-21 22:25:59 +0300

    Coinbase Wallet fix

--- src/services/web3Service.js
+++ src/services/web3Service.js
@@ -21,28 +21,23 @@ const Web3Service = () => {
 	if (!web3) {
 	    throw new Error("Web3 is not connected");
 	}
-
-	console.log("Web3 injected: ", {web3});
 	Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 	Promise.promisifyAll(web3.version, { suffix: "Promise" });
 	web3.eth.getTransactionReceiptMined = getTransactionReceiptMined;
-
 
 	const accounts = await web3.eth.getAccountsPromise();
 	const network = await web3.version.getNetworkPromise();
 	const address = accounts[0];
 
-
 	const balanceBig = address ? await web3.eth.getBalancePromise(address) : 0;
 	let balance;
 	if (balanceBig) {
-	    console.log({web3});
             balance = web3.fromWei(balanceBig, 'ether');
 	}
 
-	const connected = web3.isConnected();
+	const connected = true;//web3.isConnected();
 	const { networkName, networkId } = detectNetwork(web3);
-
+	
 	return {
 	    web3,
 	    balance,
